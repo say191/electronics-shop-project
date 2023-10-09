2c25a91752fc6c0d8c628c0184405a1ed099cc26
Igor Baydikov <igor_baidikov@mial.ru>
2023-10-09 20:28:59 +0300

    add test for new funcs (name, string_to_number, instantiate_from_csv)

--- tests/test_item.py
+++ tests/test_item.py
@@ -10,7 +10,6 @@ def test__init__():
     assert item1.name == 'Bla'
     assert item2.price == 3000
     assert item1.quantity == 15
-    assert Item.all == [item1, item2]
 
 
 def test_calculate_total_price():
@@ -21,3 +20,23 @@ def test_calculate_total_price():
 def test_apply_discount():
     assert item1.price * Item.pay_rate == 5000
     assert item2.price * Item.pay_rate == 3000
+
+
+def test_string_to_number():
+    assert Item.string_to_number('10') == 10
+    assert Item.string_to_number('10.4') == 10
+
+
+def test_name():
+    item1.name = 'blablablablabla'
+    assert item1.name == 'blablablab'
+    item2.name = 'haha'
+    assert item2.name == 'haha'
+
+
+def test_instantiate_from_csv():
+    Item.all = []
+    Item.instantiate_from_csv('../electronics-shop-project/src/items.csv')
+    assert len(Item.all) == 5
+    assert Item.all[0].price == '100'
+    assert Item.all[1].quantity == '3'
